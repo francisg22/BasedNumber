12f0c148244d7081b2bb6dbcd876ed3e98b4f540
francisg0 <8910306-francisg0@users.noreply.replit.com>
2023-09-11 15:04:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+class based_number(object):
+  def __init__(self, numberAsString, baseAsInt):
+    self.numberAsString = numberAsString
+    self.baseAsInt = baseAsInt
+    self._int_value = int(based_number.to_decimal(numberAsString, baseAsInt))
+  def to_decimal(num,base):
+    return int(num,base)
+
+
+
+if __name__ == "__main__":
+  a = based_number("01011",2)
+  print(a._int_value)
