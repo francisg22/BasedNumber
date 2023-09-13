896466ffb200632719283bd5d767f57398b46e81
francisg22 <109164838+francisg22@users.noreply.github.com>
2023-09-13 14:38:39 +0000

    transferring code to local environment

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
+def compare_with_int_value(cls):
+    def create_func(name):
+        def _do_it(self, other):
+            # print('%r %s %r' % (self, name, other))
+            if isinstance(other, type(self)):
+                return getattr(self._int_value, name)(other._int_value)
+            else:
+                return getattr(self._int_value, name)(other)
+        return _do_it
+
+    for name in ('__ge__','__gt__','__le__','__lt__','__eq__','__ne__'):
+        setattr(cls, name, create_func(name))
+
+    return cls
+@compare_with_int_value
 class based_number(object):
   def __init__(self, numberAsString, baseAsInt, sign = "p"):
     self.numberAsString = numberAsString
     self.baseAsInt = baseAsInt
-    self._int_value = int(based_number.to_decimal(numberAsString, baseAsInt))
-    self.sign = sign
+    #self._int_value = based_number.ecode(numberAsString, baseAsInt)
+    self._int_value = sum([int(c)*2**(int(k)) for k,c in enumerate(reversed(numberAsString))])
     
-  @staticmethod
-  def to_decimal(num,base):
-    return int(num,base)
-  def subtract(self, num):
-   #STRINGS HAVE TO BE THE SAME LENGTH (AS OF RN)
-   #MAKE A FUNCTION TO ADD SOME ZEROS TO THE FRONT OF THE SMALLER STRING
+    
+
+  def __getitem__(self,key):
+    return self.numberAsString[key]
+  #takes two numbers
+  #based_number object it is called on has to be greater than num passed through
+  
+  def __sub__(self, num):
     num.normalize(self)
-    
-    if(num._int_value > self._int_value):
+    if(num > self):
       return "no"
     rv = ''
     s = self.numberAsString
     s2 = num.numberAsString
     i = len(s)-1
-    # carry = [0] * (i + 1)
     while i >= 0:
-      
       a = int(s[i]) - int(s2[i])
       if(a == 1):
         rv = '1' + rv
@@ -31,54 +45,47 @@ class based_number(object):
         n = 1
         if(i-n >= 0):
           while(int(s[i-n]) >= 0 and s[i-n] == '0'):
-            
-            # s = s[:i-n] + '1' + s[i-n + 1:]
             n+=1
-          
-          
           s = s[:i-n] + '0' + s[i-n + 1:]
           for j in range(n):
             if(i == 0 or i == n-1 ):
               continue
             else:
                 s = s[:i-j] + '1' + s[i-j + 1:]
-            
           rv = '1' + rv
         else:
           rv = '1' + rv
-      
-            
-        # carry[i-1] = -1
-        
-      
-        
-      
       i -= 1
-    
-    
-    
     return rv
     
-  # def division(self,num):
-  #   if(self._int_value < num._int_value):
-  #     return "Number less than 0"
-  #   rv = ""
-  #   current = ""
-  #   pos = 0
-  #   # print(self.numberAsString)
-  #   # print(self.numberAsString[pos:pos+len(num.numberAsString)])
-  #   # while len(current)< len(num.numberAsString):
+  def __add__(self,num):
+    pass
     
-  #   current = based_number(self.numberAsString[pos:pos+len(num.numberAsString)],2)
+  def left_shift_and_add(self,num):
+    return based_number(self.numberAsString + num,2)
     
-  #   # print(current.numberAsString + ' '+ str(current._int_value))
-  #   temp = current.subtract(num)
-  #   if(temp == 'no'):
-  #     current = based_number(self.numberAsString[pos:pos+1+len(num.numberAsString)],2)
-  #   # print(current.numberAsString + ' '+ str(current._int_value))
-  #   # print(num.numberAsString + ' ' +str(num._int_value))
-  #   temp = current.subtract(num)
-    # print( temp + ' ' + str(based_number.to_decimal(temp,2)))
+  def division(self,num):
+    if(self < num):
+      return "Number less than 1"
+    rv = "0"
+    i= 1
+    remainder = based_number('0',2)
+    
+    while(i<self.numberAsString):
+      while(remainder < num):
+        remainder = self.numberAsString[0:i]
+        i+=1
+        rv += '0'
+      temp = remainder - num
+      rv += '1'
+      
+    
+    
+    
+      
+      
+      
+     
       
   def normalize(self,num):
     if(len(self.numberAsString)<len(num.numberAsString)):
@@ -89,6 +96,13 @@ class based_number(object):
       diff = len(self.numberAsString) - len(num.numberAsString)
       add = '0' * diff
       num.numberAsString = add + self.numberAsString
+  def compare(self,num):
+    if(self._int_value > num._int_value):
+      return True
+    if (self._int_value == num._int_value):
+      return True
+    if(self._int_value < num._int_value):
+      return True
     
       
       
@@ -100,18 +114,24 @@ class based_number(object):
 if __name__ == "__main__":
   a = based_number("10010000",2)
   b = based_number("1100",2)
-  c = based_number("11010010",2)
-  d = based_number("1101010",2)
-  print(c._int_value)
-  print(d._int_value)
-  
-  f = c.subtract(d)
-  print('c - d=' + f)
-  f = based_number(f,2)
-  print(f.numberAsString)
+  c = based_number("10000001",2)
+  d = based_number("1111111",2)
+  # print(c._int_value)
+  # print(d._int_value)
+  # print(c-d)
+  # f = c<d
+  # print('f= ' + str(f))
+  # f = c.subtract(d)
+  # print('c - d=' + f)
+  # f = based_number(f,2)
+  # print(f.numberAsString)
   # print(based_number.to_decimal(f,2))
-
+  print("a = " + a.numberAsString)
+  print("b = " +b.numberAsString)
+  print("a = " + str(a._int_value))
+  print("b = " + str(b._int_value))
   # b.normalize(a)
-  # print(a.division(b))
-  print(a._int_value)
-  print(b._int_value)
+  print(a[i])
+  print(a[i:i+1])
+  print(a.division(b))
+  
